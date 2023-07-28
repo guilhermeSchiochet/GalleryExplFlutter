c1f6f10d48e3512dc294b0836ed57c96e90b0581
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2023-07-27 21:01:31 -0300

    Create list_images.dart

---
+++ lib/src/utils/resources/list_images.dart
@@ -0,0 +1,11 @@
+class ListItens {
+  static const List<String> itens = [
+    'assets/images/image_1.jpg',
+    'assets/images/image_2.jpg',
+    'assets/images/image_3.jpg',
+    'assets/images/image_4.jpg',
+    'assets/images/image_5.jpg',
+    'assets/images/image_6.jpg',
+    'assets/pdfs/pdf_example.pdf',
+  ];
+}
